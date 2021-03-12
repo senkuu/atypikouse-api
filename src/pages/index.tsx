2ae4fc98458b4632f10dbd5655f2a571d8cdde3b
Enzo Do rosario <dorosario.enzo@gmail.com>
2021-03-12 15:41:07 +0100

    feat(landing): finish section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 
 import tw, { styled } from "twin.macro";
 
@@ -9,11 +10,27 @@ const MainContainer = styled.main`
   background: #F9F7F2;
 `;
 
-const Container = tw.div`container mx-auto flex flex-col justify-center p-6 sm:py-12 lg:py-24 lg:flex-row lg:justify-between`;
+const Container = tw.div`container mx-auto flex flex-col justify-center p-6 sm:py-12 lg:py-24`;
 
 const Title = tw.h2`font-serif text-5xl md:text-6xl uppercase`;
 
 const GreenText = tw.span`text-green-500`;
+
+const ImagesContainer = tw.div`flex flex-col mt-6 mb-4 lg:mb-0 lg:flex-row lg:justify-around sm:mt-12`;
+
+const ImageSquareContainer = tw.div`relative`;
+
+const Square = tw.div`absolute bg-green-500 h-36 w-36`;
+
+const BottomLeftSquare = styled(Square)`
+  left: -10px;
+  bottom: -5px;
+`;
+
+const TopRightSquare = styled(Square)`
+  right: -10px;
+  top: -10px;
+`;
 
 export default function Home() {
   return (
@@ -27,6 +44,26 @@ export default function Home() {
           <Title>
             des lieux <GreenText>d’exceptions</GreenText>
           </Title>
+          <ImagesContainer>
+            <ImageSquareContainer>
+              <BottomLeftSquare />
+              <Image
+                src="/landing-left.png"
+                alt="Photo de maison atypique"
+                width={557}
+                height={371}
+              />
+            </ImageSquareContainer>
+            <ImageSquareContainer>
+              <TopRightSquare />
+              <Image
+                src="/landing-right.jpg"
+                alt="Photo de maison atypique"
+                width={557}
+                height={371}
+              />
+            </ImageSquareContainer>
+          </ImagesContainer>
         </Container>
       </MainContainer>
     </>
