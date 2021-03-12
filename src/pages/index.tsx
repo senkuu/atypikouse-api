81b04e7c3b530488c591fd716f8407728e0293da
Enzo Do rosario <dorosario.enzo@gmail.com>
2021-03-12 09:40:45 +0100

    fix: accessibility problem
    Add title for index page and lang attribute to html

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,9 @@ import HomeHero from "../components/HomeHero";
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Atypik'House | Une séléction de lieux insolites premiums</title>
+      </Head>
       <HomeHero />
       <h1>Atypik'house</h1>
     </>
