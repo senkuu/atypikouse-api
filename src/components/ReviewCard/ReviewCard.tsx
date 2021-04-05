7862dc001a9b40c3cb4f025fa7ed2484b0dad826
Enzo Do rosario <dorosario.enzo@gmail.com>
2021-04-05 19:46:20 +0200

    first landing

---
+++ src/components/ReviewCard/ReviewCard.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Image from "next/image";
+
+import tw, { styled } from "twin.macro";
+
+interface Author {
+  name: string;
+  location: string;
+  profilePictureURI: string;
+}
+
+interface props {
+  author: Author;
+  review: string;
+}
+
+const Card = tw.figure`bg-white shadow-lg p-8 md:flex md:p-0 lg:w-3/4 lg:h-64`;
+
+const ProfilePics = tw.img`w-32 h-32 rounded-full mx-auto md:w-1/4 md:h-auto md:rounded-none`;
+
+function ReviewCard(props: props) {
+  return (
+    <Card>
+      <ProfilePics src={props.author.profilePictureURI} />
+      <div tw="pt-6 text-center space-y-4 md:text-left md:p-8">
+        <blockquote>
+          <p tw="text-lg font-semibold">“{props.review}”</p>
+        </blockquote>
+        <figcaption tw="font-medium">
+          <div tw="text-green-600">{props.author.name}</div>
+          <div tw="text-gray-500">{props.author.location}</div>
+        </figcaption>
+      </div>
+    </Card>
+  );
+}
+
+export default ReviewCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,21 +4,24 @@ import Image from "next/image";
 import tw, { styled } from "twin.macro";
 
 import HomeHero from "../components/HomeHero";
+import ReviewCard from "../components/ReviewCard";
 
 const MainContainer = styled.main`
   ${tw`w-screen`}
   background: #F9F7F2;
 `;
 
-const Container = tw.div`container mx-auto flex flex-col justify-center p-6 sm:py-12 lg:py-24`;
+const Container = tw.div`container mx-auto flex flex-col justify-center p-6 sm:py-12 lg:px-24 lg:py-24`;
 
-const Title = tw.h2`font-serif text-5xl md:text-6xl uppercase`;
+const Title = tw.h2`font-serif text-5xl text-gray-800 md:text-6xl uppercase`;
 
 const GreenText = tw.span`text-green-500`;
 
-const ImagesContainer = tw.div`flex flex-col mt-6 mb-4 lg:mb-0 lg:flex-row lg:justify-around sm:mt-12`;
+const ResponsiveContainer = tw.div`flex flex-col mt-6 mb-4 gap-8 lg:mb-0 lg:flex-row lg:justify-between sm:mt-12`;
 
-const ImageSquareContainer = tw.div`relative`;
+const ReviewsContainer = tw.div`flex flex-col gap-8 lg:items-center`;
+
+const ImageSquareContainer = tw.div`relative w-max`;
 
 const Square = tw.div`absolute bg-green-500 h-36 w-36`;
 
@@ -32,6 +35,8 @@ const TopRightSquare = styled(Square)`
   top: -10px;
 `;
 
+const Text = tw.p`font-sans text-gray-600`;
+
 export default function Home() {
   return (
     <>
@@ -41,29 +46,77 @@ export default function Home() {
       <HomeHero />
       <MainContainer>
         <Container>
-          <Title>
-            des lieux <GreenText>d’exceptions</GreenText>
-          </Title>
-          <ImagesContainer>
-            <ImageSquareContainer>
-              <BottomLeftSquare />
-              <Image
-                src="/landing-left.png"
-                alt="Photo de maison atypique"
-                width={557}
-                height={371}
+          <section>
+            <Title>
+              des lieux <GreenText>d’exceptions</GreenText>
+            </Title>
+            <ResponsiveContainer>
+              <ImageSquareContainer>
+                <BottomLeftSquare />
+                <Image
+                  src="/landing-left.png"
+                  alt="Photo de maison atypique"
+                  width={557}
+                  height={371}
+                />
+              </ImageSquareContainer>
+              <ImageSquareContainer>
+                <TopRightSquare />
+                <Image
+                  src="/landing-right.jpg"
+                  alt="Photo de maison atypique"
+                  width={557}
+                  height={371}
+                />
+              </ImageSquareContainer>
+            </ResponsiveContainer>
+          </section>
+          <section tw="mt-24">
+            <Title>Fini les mauvaises surprises !</Title>
+            <ResponsiveContainer>
+              <Text tw="max-w-sm md:max-w-xl text-2xl text-justify self-center">
+                Atypik’house ne propose qu’une sélection de lieux premium{" "}
+                <GreenText>testés</GreenText> et{" "}
+                <GreenText>approuvés</GreenText> par nos soins afin de vous
+                éviter au maximum les mauvaises surprises lors de votre arrivé.
+                <br />
+                <br />
+                Une réservation sans prise de tête !
+              </Text>
+              <ImageSquareContainer>
+                <TopRightSquare />
+                <Image
+                  src="/landing-right.jpg"
+                  alt="Photo de maison atypique"
+                  width={557}
+                  height={371}
+                />
+              </ImageSquareContainer>
+            </ResponsiveContainer>
+          </section>
+          <section tw="mt-24">
+            <Title tw="mb-16 text-center">Vous en parlez mieux que nous</Title>
+            <ReviewsContainer>
+              <ReviewCard
+                author={{
+                  name: "Nathalie D.",
+                  location: "Seine-Maritime",
+                  profilePictureURI:
+                    "https://cdn-elle.ladmedia.fr/var/plain_site/storage/images/people/la-vie-des-people/news/natalie-portman-declare-sa-flamme-a-benjamin-millepied-3868407/93600559-1-fre-FR/Natalie-Portman-declare-sa-flamme-a-Benjamin-Millepied.jpg",
+                }}
+                review="Logement réservé la veille pour le weekend, tout était conforme à nos attentes ! Anne-Claude nous a même fait bénéficier d’un petit-déjeuner gratuit, c’était notre premier weekend avec Atypik’house, mais sûrement pas le dernier !"
               />
-            </ImageSquareContainer>
-            <ImageSquareContainer>
-              <TopRightSquare />
-              <Image
-                src="/landing-right.jpg"
-                alt="Photo de maison atypique"
-                width={557}
-                height={371}
+              <ReviewCard
+                author={{
+                  name: "Cyril H.",
+                  location: "Eure",
+                  profilePictureURI:
+                    "https://www.nextplz.fr/wp-content/uploads/2020/03/tpmp-cyril-hanouna-dvoile-qui-est-la-pire-clbrit-quil-a-reu-en-plateau.jpg",
+                }}
+                review="C’était la grosse dehka !!! Le logement était incroyable les amis !! On s’est éclaté avec Delormeau et mon pote Gilles, on va refaire ça HAHHAHAHAH C’EST LA DEHKA"
               />
-            </ImageSquareContainer>
-          </ImagesContainer>
+            </ReviewsContainer>
+          </section>
         </Container>
       </MainContainer>
     </>
